d2422b2197e01a7205219464ca0f4ef7f2beb54a
Jörgen Bjerkesjö <jorgen.bjerkesjo@gmail.com>
2023-08-24 09:25:27 +0200

    Fixed html_theme in config file

--- conf.py
+++ conf.py
@@ -135,6 +135,7 @@ def setup(app):
         app.add_js_file('wedc.js?v=4')
 
 #html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
